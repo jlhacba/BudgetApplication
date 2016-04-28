efd336c53a4cd39735d6474d35d3c76619255bbb
jlha <jlha@crimson.ua.edu>
2016-04-28 10:40:55 -0500

    Morris.js donut showed up! Layout files had to have scripts added, size isn't right yet

--- BudgetApp/Controllers/HomeController.cs
+++ BudgetApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BudgetApp.Models;
+using BudgetApp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,8 +8,30 @@ using System.Web.Mvc;
 
 namespace BudgetApp.Controllers
 {
+
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult GetCompleted()
+        {
+            var pf = new List<Donut>();
+
+            var pass = new Donut();
+            pass.label = "Completed";
+            pass.value = "28";
+
+            var fail = new Donut();
+            fail.label = "Yet to Complete";
+           
+            fail.value = (db.Categories.Count().ToString());
+
+            pf.Add(pass);
+            pf.Add(fail);
+
+            return Json(pf, JsonRequestBehavior.AllowGet);
+        }
+
         public ActionResult Index()
         {
             return View();
